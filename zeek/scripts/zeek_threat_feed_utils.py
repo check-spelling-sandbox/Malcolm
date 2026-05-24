9452a827c110e026e257076f3ee8ff197c033d24
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-05-24 19:38:03 -0400

    spelling: response
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- zeek/scripts/zeek_threat_feed_utils.py
+++ zeek/scripts/zeek_threat_feed_utils.py
@@ -1093,14 +1093,14 @@ def UpdateFromMISP(
                             else defaultNow
                         )
                         if (since is None) or (eventTime >= since):
-                            mispObjectReponse = mispSession.get(
+                            mispObjectResponse = mispSession.get(
                                 newUrl,
                                 allow_redirects=True,
                                 verify=sslVerify,
                             )
-                            mispObjectReponse.raise_for_status()
+                            mispObjectResponse.raise_for_status()
                             if zeekPrinter.ProcessMISP(
-                                mispObjectReponse.json(),
+                                mispObjectResponse.json(),
                                 url=newUrl,
                             ):
                                 successCount.increment()
@@ -1142,14 +1142,14 @@ def UpdateFromMISP(
                                 )
                                 if (since is None) or (eventTime >= since):
                                     newUrl = f'{mispUrl.strip("/")}/{uri}.json'
-                                    mispObjectReponse = mispSession.get(
+                                    mispObjectResponse = mispSession.get(
                                         newUrl,
                                         allow_redirects=True,
                                         verify=sslVerify,
                                     )
-                                    mispObjectReponse.raise_for_status()
+                                    mispObjectResponse.raise_for_status()
                                     if zeekPrinter.ProcessMISP(
-                                        mispObjectReponse.json(),
+                                        mispObjectResponse.json(),
                                         url=newUrl,
                                     ):
                                         successCount.increment()
@@ -1167,14 +1167,14 @@ def UpdateFromMISP(
                 # just loop over, retrieve and process the .json files in this directory
                 for url in paths:
                     try:
-                        mispObjectReponse = mispSession.get(
+                        mispObjectResponse = mispSession.get(
                             url,
                             allow_redirects=True,
                             verify=sslVerify,
                         )
-                        mispObjectReponse.raise_for_status()
+                        mispObjectResponse.raise_for_status()
                         if zeekPrinter.ProcessMISP(
-                            mispObjectReponse.json(),
+                            mispObjectResponse.json(),
                             url=url,
                         ):
                             successCount.increment()
